780fbfad40dee0d19b2b142cfb9c5f977c3452e9
Barry Hanna <barryahanna@gmail.com>
2023-06-08 16:04:00 +0100

    Add message form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/new', function (req, res) {
+	res.render('new', { title: 'Mini Message Board' });
+});
+
+router.post('/new', function (req, res) {
+	console.log(req.body);
+	res.status(200).send(`OK!`);
+});
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+	res.render('index', { title: 'Mini Message Board' });
 });
 
 module.exports = router;
