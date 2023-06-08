0fbae3d9c6955dc570cf7fe851c5c424fab2b6d8
Barry Hanna <barryahanna@gmail.com>
2023-06-08 16:47:51 +0100

    Add function to add messages

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,50 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+	{
+		text: 'Hi, there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
+
 router.get('/new', function (req, res) {
 	res.render('new', { title: 'Mini Message Board' });
 });
 
 router.post('/new', function (req, res) {
+	messages.push({
+		text: req.body.message,
+		user: req.body.username,
+		added: new Date(),
+	});
 	console.log(req.body);
-	res.status(200).send(`OK!`);
+	res.redirect('/');
 });
 
 /* GET home page. */
 router.get('/', function (req, res) {
-	res.render('index', { title: 'Mini Message Board' });
+	const formattedMessages = messages.map(({ text, user, added }) => {
+		return {
+			text,
+			user,
+			added: added.toLocaleDateString('en-GB', {
+				weekday: 'long',
+				month: 'long',
+				day: 'numeric',
+			}),
+		};
+	});
+	res.render('index', {
+		title: 'Mini Message Board',
+		messages: formattedMessages,
+	});
 });
 
 module.exports = router;
